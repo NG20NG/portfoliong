14551de45996f5b80c4e3e645867911d1d6c23be
unknown <ngnassim20@gmail.com>
2022-09-20 22:31:45 +0200

    'update'

--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -3,8 +3,18 @@ import p from "./projects.module.css";
 
 const Project = () => {
   return (
-    <div className={p.projectsContainer}>
-      <div></div>
+    <div className={p.projects}>
+      <div className={p.secondProjects}>
+        <div className={p.cards}>
+          <div></div>
+        </div>
+        <div className={p.cards}>
+          <div></div>
+        </div>
+        <div className={p.cards}>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
